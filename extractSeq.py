dff6d48fe4a2c8176900bdc0dbb8e2dda5de0b03
Lin Xue 林 雪 <flosalbizziae@users.noreply.github.com>
2018-05-09 08:57:27 +0800

    Update extractSeq.py

--- extractSeq.py
+++ extractSeq.py
@@ -3,17 +3,14 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from collections import defaultdict
 
-# read names and postions from bed file
 positions = defaultdict(list)
 with open('T47D-TRPS1_peaks.bed') as f:
     for line in f:
         name, start, stop = line.split()
         positions[name].append((int(start), int(stop)))
 
-# parse faste file and turn into dictionary
 records = SeqIO.to_dict(SeqIO.parse(open('../../REF/hg38/hg38.fa'), 'fasta'))
 
-# search for short sequences
 short_seq_records = []
 for name in positions:
     for (start, stop) in positions[name]:
@@ -24,6 +21,5 @@ for name in positions:
         short_seq_record = SeqRecord(Seq(short_seq, alphabet), id=name, description='')
         short_seq_records.append(short_seq_record)
 
-# write to file
 with open('T47D-TRPS1_peaks.fasta', 'w') as f:
     SeqIO.write(short_seq_records, f, 'fasta')
